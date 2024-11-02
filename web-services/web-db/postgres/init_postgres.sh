22345cb663ea15ea78aef4cf4d85ec9593b19c0a
m-spangenberg <mspangenberg.dev@gmail.com>
2024-11-02 18:49:24 +0200

    chore: update

--- web-services/web-db/postgres/init_postgres.sh
+++ web-services/web-db/postgres/init_postgres.sh
@@ -3,10 +3,23 @@
 set -e
 
 # Wait for PostgreSQL to be ready
-until psql -h "localhost" -U "postgres" -c '\q'; do
-  echo "Waiting for PostgreSQL to be ready..."
+until pg_isready -h localhost -U ${POSTGRES_USER} -p 5432; do
+  case $? in
+    1)
+      echo "PostgreSQL is rejecting connections. Waiting..."
+      ;;
+    2)
+      echo "No response from PostgreSQL. Waiting..."
+      ;;
+    3)
+      echo "Invalid parameters for pg_isready. Exiting..."
+      exit 1
+      ;;
+  esac
   sleep 2
 done
+
+echo "PostgreSQL is ready to accept connections."
 
 # Create gitea user and database
 psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
