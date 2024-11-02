f1d94eb241716a46f4a549fb57786e9f655ddc83
m-spangenberg <mspangenberg.dev@gmail.com>
2024-11-02 19:18:35 +0200

    chore: update

--- web-services/web-db/postgres/init_postgres.sh
+++ web-services/web-db/postgres/init_postgres.sh
@@ -5,7 +5,7 @@ set -e
 echo "Initializing Service Databases..."
 
 # Create gitea user and database
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" -h postgres <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" -h database <<-EOSQL
   CREATE USER ${GITEA_DB_USER} WITH PASSWORD '${GITEA_DB_PASSWORD}';
   CREATE DATABASE ${GITEA_DB_NAME} OWNER ${GITEA_DB_USER};
   GRANT ALL PRIVILEGES ON DATABASE ${GITEA_DB_NAME} TO ${GITEA_DB_USER};
@@ -14,7 +14,7 @@ EOSQL
 echo "gitea user and database created successfully."
 
 # Create woodpecker user and database
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" -h postgres <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" -h database <<-EOSQL
   CREATE USER ${WOODPECKER_DB_USER} WITH PASSWORD '${WOODPECKER_DB_PASSWORD}';
   CREATE DATABASE ${WOODPECKER_DB_NAME} OWNER ${WOODPECKER_DB_USER};
   GRANT ALL PRIVILEGES ON DATABASE ${WOODPECKER_DB_NAME} TO ${WOODPECKER_DB_USER};
